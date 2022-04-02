adba087ba1bdd5a62c37a8458e3436ffc1e8c1d8
mcerize <mateusdc@gmail.com>
2022-04-02 02:36:14 -0300

    criando projeto

---
+++ src/test/java/com/housekeeper/api/HousekeeperApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.housekeeper.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HousekeeperApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
